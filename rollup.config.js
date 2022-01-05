e866f5db69fbeb1ec13c8b99f6d77c65c423703b
Valentin Hervieu <valentin@hervi.eu>
2022-01-05 21:21:06 +0100

    fix: simplify rollup config to support CodeSandbox

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,11 @@ import svelte from 'rollup-plugin-svelte'
 import resolve from '@rollup/plugin-node-resolve'
 import pkg from './package.json'
 
-const input = 'src/index.svelte'
-
-export default [
-  {
-    input,
-    output: { file: pkg.main, format: 'umd', name: 'Cropper' },
-    plugins: [svelte(), resolve()],
-  },
-  {
-    input,
-    output: { file: pkg.module, format: 'es' },
-    external: ['svelte/internal'],
-    plugins: [svelte()],
-  },
-]
+export default {
+  input: 'src/index.svelte',
+  output: [
+    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'umd', name: 'Cropper' },
+  ],
+  plugins: [svelte(), resolve()],
+}
