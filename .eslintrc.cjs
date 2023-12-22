309b377695f5c68caf1f9c63a1948dcef6196383
dhernando <dhernando@newrelic.com>
2023-12-22 15:42:05 +0100

    chore: bump sveltekit to v2 along with its dependencies, add tabindex and role=button to avoid aria warnings and fix package publishing to tackle the https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#missing-exports-condition issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,27 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint', 'cypress'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-  settings: {
-    'svelte3/typescript': () => require('typescript'),
-  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:svelte/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'cypress'],
+  ignorePatterns: ['*.cjs', 'dist/*'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
   },
   env: {
     browser: true,
