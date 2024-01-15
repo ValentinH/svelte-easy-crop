90882a9064f5236b0b7c2c8e6160906eda47a273
drijnkels <daanrijnkels@gmail.com>
2024-01-15 08:38:22 -0700

    add minZoom & maxZoom to test page to be able to test zoom less than 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,18 @@
 
   let crop = { x: 0, y: 0 }
   let zoom = 1
+  let minZoom = 0.5;
+  let maxZoom = 3;
 
   const urlArgs = typeof window !== 'undefined' ? queryString.parse(window.location.search) : null
   let image = typeof urlArgs?.img === 'string' ? urlArgs.img : '/images/dog.jpeg' // so we can change the image from our tests
 </script>
 
-<Cropper {image} bind:crop bind:zoom on:cropcomplete={e => console.log(e.detail)} />
+<Cropper
+  {image}
+  bind:crop
+  bind:zoom
+  bind:minZoom
+  bind:maxZoom
+  on:cropcomplete={e => console.log(e.detail)}
+/>
